558732949745bcd119cd6842df0e78150913a0c7
ChooGyohan <gyohan0507@gmail.com>
2017-10-12 10:44:34 +0900

    make website

--- project.py
+++ project.py
@@ -1,10 +1,10 @@
 import sys
-sys.path.append('D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emiles_Website_Project')
-
-from input import login, writing, comment, uploading
+sys.path.append('D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emiles_Website_Project\input')
+sys.path.append('D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emiles_Website_Project\process')
+sys.path.append('D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emiles_Website_Project\output')
+#from input import login, writing, comment, uploading
 from process import deleting
 from output import downloading
-
 
 
 
