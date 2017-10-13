d296b976562b3e80cd5e341c8a8d0b8bfa1ff9d0
ChooGyohan <gyohan0507@gmail.com>
2017-10-14 01:28:08 +0900

    Revision

--- project.py
+++ project.py
@@ -1,13 +1,10 @@
+
 import sys
-#sys.path.append("D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emilles_Website_Project\Input")
-sys.path.append("D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emilles_Website_Project\Output")
-sys.path.append("D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emilles_Website_Project\Process")
-#import Input
+sys.path.append("D:/cnrygks/Sogang University/2017_4학기 ()/일반교양_기초응용소프트웨어프로그래밍/Emilles_Website_Project/Input")
+sys.path.append("D:/cnrygks/Sogang University/2017_4학기 ()/일반교양_기초응용소프트웨어프로그래밍/Emilles_Website_Project/Output")
+sys.path.append("D:/cnrygks/Sogang University/2017_4학기 ()/일반교양_기초응용소프트웨어프로그래밍/Emilles_Website_Project/Process")
+
+import Input
 import Output
 import Process
 
-print(dir(Input))
-
-Input.login()
-Output.downloading()
-Process.deleting()
