ef8ce22c9d7e43c38daf8b4b1e25bf2ede8bfba9
ChooGyohan <gyohan0507@gmail.com>
2017-10-14 01:11:53 +0900

    First

--- project.py
+++ project.py
@@ -1,17 +1,13 @@
-import Input
-import Process
+import sys
+#sys.path.append("D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emilles_Website_Project\Input")
+sys.path.append("D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emilles_Website_Project\Output")
+sys.path.append("D:\cnrygks\Sogang University\2017_4학기 ()\일반교양_기초응용소프트웨어프로그래밍\Emilles_Website_Project\Process")
+#import Input
 import Output
+import Process
 
 print(dir(Input))
-print(dir(Output))
-print(dir(Process))
 
 Input.login()
-#Input.writing()
-#Process.deleting()
-#Output.downloading()
-
-
-
-
-
+Output.downloading()
+Process.deleting()
